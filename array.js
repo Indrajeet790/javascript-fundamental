d936a2cacaf6c3c990bea7f274cddb4459d78cec
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-01-14 21:51:31 +0530

    validation code

--- array.js
+++ array.js
@@ -1,5 +1,21 @@
 // Array traversing and accessing
 let data = [9, 45, 2, 8, 45, 23, 7, 78, 0, 11, 41, 77];
-for (let i = 0; i < data.length; i++) {
+/*
+// Traversing with loop
+//for (let i = 0; i < data.length; i++) {
   console.log(` Array${i} is ${data[i]}`);
+}
+
+*/
+// access element with text input and button
+// let x = 1;
+// console.log(data[x]);
+// validation
+function getElement() {
+  let ele = document.getElementById("element").value;
+  if (ele < data.length && typeof parseInt(ele) === "number") {
+    alert(data[ele]);
+  } else {
+    alert("please enter valid input");
+  }
 }
