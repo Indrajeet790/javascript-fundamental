08a0c556be50185be3874e8f2a144cff2592c1bb
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-01-17 23:32:14 +0530

    insert element code

--- arrayinsert.js
+++ arrayinsert.js
@@ -4,3 +4,17 @@
 4.insert element with text input and button
 5.insert  element with by default function
 */
+let data = [60, 30, 10, 67, 40];
+let newEl = 70;
+let position = 2;
+console.warn(data);
+for (let i = data.length - 1; i >= 0; i--) {
+  // console.log(data[i]);
+  if (i >= position) {
+    data[i + 1] = data[i];
+    if (i === position) {
+      data[i] = newEl;
+    }
+  }
+}
+console.warn(data);
