67bf3717926a424de91efa05a25565e4b95ce516
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-01-18 17:11:33 +0530

    dynamic program inserting element in array

--- arrayinsert.js
+++ arrayinsert.js
@@ -4,6 +4,7 @@
 4.insert element with text input and button
 5.insert  element with by default function
 */
+/*
 let data = [60, 30, 10, 67, 40];
 let newEl = 70;
 let position = 2;
@@ -18,3 +19,22 @@ for (let i = data.length - 1; i >= 0; i--) {
   }
 }
 console.warn(data);
+*/
+// dynamic program
+function insertEl() {
+  let data = [60, 30, 10, 67, 40];
+  let newEl = document.getElementById("newEl").value;
+  newEl = parseInt(newEl);
+  let position = document.getElementById("position").value;
+  console.warn(data);
+  for (let i = data.length - 1; i >= 0; i--) {
+    // console.log(data[i]);
+    if (i >= position) {
+      data[i + 1] = data[i];
+      if (i == position) {
+        data[i] = newEl;
+      }
+    }
+  }
+  console.warn(data);
+}
