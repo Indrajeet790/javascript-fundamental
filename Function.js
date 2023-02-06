8e57bad779bbba9942bb9436cb0ab31be84256d7
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-02-06 21:09:40 +0530

    default parameter

---
+++ Function.js
@@ -0,0 +1,25 @@
+"use strict";
+//  default parameters
+/*
+const bookings = [];
+
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 299 * numPassengers
+) {
+  // ES5
+  // numPassengers = numPassengers || 1;
+  // price = price || 299;
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+createBooking("LH123");
+createBooking("LH123", 3, 1000);
+createBooking("LH123", 5);
+*/
