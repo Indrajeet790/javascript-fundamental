d77a034d23d9f34c04a11035f76f18f0e3bf2706
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-02-08 10:58:27 +0530

    promise.js

--- promise.js
+++ promise.js
@@ -1,32 +1,96 @@
-function register(callback) {
-  setTimeout(() => {
-    console.log("Register end");
-    callback();
-  }, 1000);
-}
-function sendEmail(callback) {
-  setTimeout(() => {
-    console.log("Email end");
-    callback();
-  }, 2000);
-}
-
-function login(callback) {
-  setTimeout(() => {
-    console.log("login end");
-    callback();
-  }, 3000);
+// function register() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log("Register end");
+//       resolve();
+//     }, 1000);
+//   });
+// }
+function register() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Register end");
+      resolve("success");
+    }, 1000);
+  });
 }
 
-function getUserData(callback) {
-  setTimeout(() => {
-    console.log("Got user data");
-    callback();
-  }, 1000);
+function sendEmail() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      // return reject("error while sending email");
+      console.log("Email end");
+      resolve();
+    }, 1000);
+  });
+}
+
+function login() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("login end");
+      resolve();
+    }, 1000);
+  });
+}
+
+function getUserData() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Got user data");
+      resolve();
+    }, 1000);
+  });
 }
 
 function displayUserData() {
-  setTimeout(() => {
-    console.log("User data display");
-  }, 1000);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("User data display");
+      resolve();
+    }, 1000);
+  });
 }
+
+// promise
+// register().then(()=>{});
+// register()
+//   .then(sendEmail)
+//   .then(login)
+//   .then(getUserData)
+//   .then(displayUserData)
+//   .catch((err) => {
+//     console.log("Error:", err);
+//   });
+
+// Async await
+async function authenticate() {
+  try {
+    const message = await register();
+    await sendEmail();
+    await login();
+    await getUserData();
+    await displayUserData();
+    console.log(message);
+  } catch (err) {
+    console.log(err);
+    throw new Error();
+  }
+}
+
+// async function authenticate() {
+//   await register();
+//   await sendEmail();
+//   await login();
+//   await getUserData();
+//   await displayUserData();
+// }
+
+// //  error handling
+authenticate()
+  .then(() => {
+    console.log("All set!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
